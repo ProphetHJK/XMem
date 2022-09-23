5f4b7ca1b4bd99017dadd134675c2959a9562e47
ProphetHJK <prophethjk@gmail.com>
2022-09-23 13:36:06 +0800

    优化绿幕视频生成

--- tools/greenback.py
+++ tools/greenback.py
@@ -1,12 +1,18 @@
-from turtle import back
 import cv2
 import numpy as np
-import cupy as cp
 import glob
 import configparser,sys
 from PIL import Image
 import os,re
 import resize,time
+import ffmpeg
+
+cupyflag = True
+try:
+    import cupy as cp
+except ImportError:
+    print("Can't find cupy，see readme.md for more imformation.")
+    cupyflag = False
 
 """
 本程序用于生成绿幕视频，方便PR等视频剪辑工具导入
@@ -21,6 +27,11 @@ def convert_str_to_float(s: str) -> float:
         num, denom = s.split('/')
         return float(num) / float(denom)
     return float(s)
+
+def print_all_numpy(n,file):
+    np.set_printoptions(threshold=np.inf)
+    with open(file, "w+") as external_file:
+        print(n, file=external_file)
 
 start = time.time()
 
@@ -56,12 +67,20 @@ if backimg_path is not None:
     backimg_file.close()
     backimg = backimg.resize(((w, h)))
     backimg = np.array(backimg)
+    if cupyflag:
+        backimg = cp.asarray(backimg)
 
 # videowriter 
 res = (w, h)
-# 获取帧率
-outstr = "".join(os.popen("ffprobe -v quiet -show_streams -select_streams v:0 %s |grep \"r_frame_rate\"" % src_file))
-framerate = re.search("r_frame_rate=(.*)",outstr).group(1)
+# 获取帧率,TODO:用r_frame_rate还是avg_frame_rate
+videoinfo = ffmpeg.probe(src_file)
+vs = next(c for c in videoinfo['streams'] if c['codec_type'] == 'video')
+framerate = vs['r_frame_rate']
+total_framenum = vs['nb_frames']
+# old
+# outstr = "".join(os.popen("ffprobe -v quiet -show_streams -select_streams v:0 %s |grep \"r_frame_rate\"" % src_file))
+# framerate = re.search("r_frame_rate=(.*)",outstr).group(1)
+
 fr = convert_str_to_float(framerate)
 fourcc = cv2.VideoWriter_fourcc(*'I420')
 if os.path.exists(dst_file+'.avi'):
@@ -69,9 +88,10 @@ if os.path.exists(dst_file+'.avi'):
 out = cv2.VideoWriter(dst_file+'.avi',fourcc, fr, res)
 
 # 获取总帧数
-outstr = "".join(os.popen("ffprobe -v quiet -show_streams -select_streams v:0 %s |grep \"nb_frames\"" % src_file))
-total_framenum = re.search("nb_frames=(.*)",outstr).group(1)
-total_framenum = int(total_framenum) - 1
+# outstr = "".join(os.popen("ffprobe -v quiet -show_streams -select_streams v:0 %s |grep \"nb_frames\"" % src_file))
+# total_framenum = re.search("nb_frames=(.*)",outstr).group(1)
+
+max_framenum = int(total_framenum) - 1
 
 frame_num = 0
 
@@ -80,18 +100,23 @@ print('初始化完成：用时：{}'.format(end))
 start = time.time()
 
 # loop
-done = False
-while not done:
-    # get frame,第一帧之前读过了,跳过读取
+while True:
+    ret = 1
+    # get frame
     if frame_num != 0:
         ret, img = cap.read()
-    else:
-        ret = 1
+    else: 
+        # 第一帧之前读过了,跳过读取
         img = frame
+    
     if not ret:
-        done = True
-        continue
-    print('进度：%d/%d\r' % (frame_num,total_framenum),end='')
+        # 全部读取完成
+        print('All frames have been read,break now')
+        break
+    if frame_num >= len(red_mask_list):
+        print('Src video length longer than mask,break now')
+        break
+    print('进度：%d/%d\r' % (frame_num,max_framenum),end='')
     red_mask = red_mask_list[frame_num]
     # print(red_mask)
     # resize,考虑加上resize.py的高斯滤波
@@ -102,55 +127,48 @@ while not done:
     red_mask_img = red_mask_img_file.convert("RGB")
     red_mask_img = np.array(red_mask_img)
     # TODO:mask图片拉伸后会有锯齿，可以尝试消除
-    red_mask_img = cv2.resize(red_mask_img, (w, h))
+    red_mask_img = cv2.resize(red_mask_img, (w, h),cv2.INTER_CUBIC)
     red_mask_img_file.close()
 
     # change to hsv
     hsv = cv2.cvtColor(red_mask_img, cv2.COLOR_RGB2HSV)
     hue,s,v = cv2.split(hsv)
 
-
-    # 提取红色构建mask矩阵，处理后mask矩阵中红色为0，非红色为255，和像素点一一对应
+    # 提取绿色（h=120）构建mask矩阵，处理后mask矩阵中绿色为255，非绿色为0，和像素点一一对应
     # 第一个参数：原始值
     # 第二个参数：lower_red指的是图像中低于这个lower_red的值，图像值变为0
     # 第三个参数：upper_red指的是图像中高于这个upper_red的值，图像值变为0
     # 而在lower_red～upper_red之间的值变成255
-    mask = cv2.inRange(hue, 0, 0)
-    # print(mask)
-    # with open("randomfile.txt", "w+") as external_file:
-    #     np.set_printoptions(threshold=np.inf)
-    #     print(mask, file=external_file)
-    #     external_file.close()
-    # sys.exit()
-
+    mask = cv2.inRange(hue, 120, 120)
+    # mask = cv2.inRange(s, 0, 0)
+    # print_all_numpy(mask,'beforesmooth.txt')
     # smooth out the mask and invert，柔化边缘，之前高斯过了，可以不用
     # kernel = np.ones((3,3), np.uint8)
-    # mask = cv2.dilate(mask, kernel, iterations = 1)
-    # mask = cv2.medianBlur(mask, 5)
-    # mask = cv2.bitwise_not(mask)
+    # # mask = cv2.dilate(mask, kernel, iterations = 1)
+    # mask = cv2.erode(mask, kernel, iterations = 1)
+    # mask = cv2.medianBlur(mask, 3)
+    # # mask = cv2.bitwise_not(mask)
+    # print_all_numpy(mask,'aftersmooth.txt')
+    # cv2.imshow("Mask", mask)
+    # if cv2.waitKey(1000) == ord('q'):
+    #     sys.exit()
 
-    # crop out the image，提取非红色部分
-    # crop = np.zeros_like(img) # 构造大小相同全0矩阵
-    # 将红色mask部分填充为原图片
-    
-    # crop = cp.asarray(img)
-    crop = img
+    if cupyflag:
+        crop = cp.asarray(img)
+    else:
+        crop = img
 
-    # with open("randomfile.txt", "w+") as external_file:
-    #     np.set_printoptions(threshold=np.inf)
-    #     print(img, file=external_file)
-    #     external_file.close()
-    # sys.exit()
     if backimg_path is None:
-        # 将非红色mask部分填充为绿色
-        crop[mask == 0] = [0,255,0] 
+        # 将绿色mask部分填充为绿色
+        crop[mask == 255] = [0,255,0] 
     else: 
-        backimg = cp.asarray(backimg)
-        # 将非红色mask部分填充为背景
+        if cupyflag:
+            backimg = cp.asarray(backimg)
+        # 将绿色mask部分填充为背景
         rows, cols = crop.shape[:2]
         for i in range(rows):
             for j in range(cols):
-                if mask[i,j] == 0:
+                if mask[i,j] == 255:
                   crop[i, j] = backimg[i, j]
 
     # show
@@ -160,7 +178,8 @@ while not done:
     # done = cv2.waitKey(1) == ord('q')
 
     # save
-    # crop = cp.asnumpy(crop)
+    if cupyflag:
+        crop = cp.asnumpy(crop)
 
     out.write(crop)
 
@@ -175,7 +194,8 @@ start = time.time()
 cap.release()
 out.release()
 
-os.system('ffmpeg -i %s -i %s -map 0:v -map 1:a? -c:v libx265 -crf 26 -c:a copy %s' % (dst_file+'.avi',src_file, dst_file))
+#  -shortest 用于音频长于视频时缩短音频长度使两者等长，libx265 crf 26质量基本不会损失画质且视频占用空间能缩小很多
+os.system('ffmpeg -i %s -i %s -map 0:v -map 1:a? -c:v libx265 -crf 26 -c:a copy -shortest %s' % (dst_file+'.avi',src_file, dst_file))
     
 end = time.time() - start
 print('mp4生成完成：用时：{}'.format(end))
